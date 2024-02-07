84cd5ac51d8ecd10fca2f1c76b6c6671156e1daa
Louie Brooks <lbrooks5@sheffield.ac.uk>
2024-02-07 10:15:28 +0000

    set up Searches.java file to write to later

---
+++ Searches.java
@@ -0,0 +1,20 @@
+public class Searches {
+
+    public static int linear(int[] data) {
+        return 0;
+    }
+
+    public static int binary(int[] data) {
+        return 0;
+    }
+
+    public static void main (String args[]) {
+
+        int[] testData = {0,9,1,8,2,7,3,6,4,5};
+
+        System.out.println(Searches.linear(testData));
+        System.out.println(Searches.binary(testData));
+
+    }
+
+}
