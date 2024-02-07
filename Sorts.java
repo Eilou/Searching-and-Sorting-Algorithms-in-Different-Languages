0864c84318b783a79936bea5ac7917f5466ee580
Louie Brooks <lbrooks5@sheffield.ac.uk>
2024-02-07 10:02:45 +0000

    set up java sorting file so I can write algorithms later

---
+++ Sorts.java
@@ -0,0 +1,33 @@
+public class Sorts {
+
+    public static int[] bubble (int[] data){
+
+        return data;
+    }
+
+    public static int[] merge (int[] data){
+
+        return data;
+    }
+
+    public static int[] insertion (int[] data){
+
+        return data;
+    }
+
+    public static int[] selection (int[] data){
+
+        return data;
+    }
+
+    public static void main (String args[]) {
+
+        int[] testData = {9,8,7,6,5,4,3,2,1};
+
+        System.out.println(Sorts.bubble(testData));
+        System.out.println(Sorts.merge(testData));
+        System.out.println(Sorts.insertion(testData));
+        System.out.println(Sorts.selection(testData));
+
+    }
+}
