bc49dec8832c6ec302c460a926df7c5e5864066d
Louie Brooks <louiewbrooks04@gmail.com>
2024-02-13 13:45:15 +0000

    both searches working now

--- java-src/Searches.java
+++ java-src/Searches.java
@@ -1,19 +1,62 @@
+/**
+ * Java sorting algorithms
+ *
+ * @author Louie Brooks
+ * Written on: starting 07/02/24
+ */
+
 public class Searches {
 
-    public static int linear(int[] data) {
-        return 0;
+    public static int linear(int[] data, int searchItem) {
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == searchItem) {
+                return i;
+            }
+        }
+
+        // returns if searchItem not in the list
+        return -1;
     }
 
-    public static int binary(int[] data) {
-        return 0;
+    public static int binary(int[] data, int searchItem) {
+
+        boolean found = false;
+
+        int start = 0;
+        int end = data.length - 1;
+        while (!found && start <= end) {
+            int mid = (start + end) / 2;
+
+            // searchItem found
+            if (data[mid] == searchItem) {
+                return mid;
+            }
+
+            else if (data[mid] < searchItem) {
+                end = mid - 1;
+            }
+
+            else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+
     }
 
     public static void main (String args[]) {
 
-        int[] testData = {0,9,1,8,2,7,3,6,4,5};
 
-        System.out.println(Searches.linear(testData));
-        System.out.println(Searches.binary(testData));
+
+        int[] unSortedTestData = {0,9,1,8,2,7,3,6,4,5};
+        int[] sortedTestData = {0,1,2,3,4,5,6,7,8,9};
+
+        int testSearchItem = -2;
+
+        System.out.println(Searches.linear(unSortedTestData, testSearchItem));
+        System.out.println(Searches.binary(sortedTestData, testSearchItem));
 
     }
 
