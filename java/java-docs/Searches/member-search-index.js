23c66d0e6243a3328fd7f06fbad53b5cd2b33856
Louie Brooks <louiewbrooks04@gmail.com>
2024-02-13 20:13:32 +0000

    javadoc'd the Searches.java file

---
+++ java/java-docs/Searches/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Searches","l":"binary(int[], int)","u":"binary(int[],int)"},{"p":"<Unnamed>","c":"Searches","l":"linear(int[], int)","u":"linear(int[],int)"},{"p":"<Unnamed>","c":"Searches","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Searches","l":"Searches()","u":"%3Cinit%3E()"}];updateSearchResults();
