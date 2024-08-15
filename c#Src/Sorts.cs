40afb4591a43beea3f15795559bf39ad9bba38f1
Louie Brooks <louiewbrooks04@gmail.com>
2024-08-15 18:50:53 +0100

    bubble sort now works

--- c#Src/Sorts.cs
+++ c#Src/Sorts.cs
@@ -5,6 +5,7 @@ namespace cSharpSrc;
 public class Sorts
 {
 
+    // runs an optimised bubble sort
     public static int[] Bubble(int[] data)
     {
         for (int i = 0; i < data.Length - 1; i++)
@@ -14,12 +15,12 @@ public class Sorts
             // optimises it, as on every pass n - 1 - i elements will be sorted
             for (int j = 1; j < data.Length - i - 1; j++)
             {
-                if (data[j-1] > data[j])
+                if (data[j] > data[j+1])
                 {
                     // used var here as could say it is ambiguous?, I'm still undecided on when best to use
-                    var temp = data[j];
-                    data[j] = data[j-1];
-                    data[j-1] = temp;
+                    var temp = data[j+1];
+                    data[j+1] = data[j];
+                    data[j] = temp;
                     swapped = true;
                     
                 }
